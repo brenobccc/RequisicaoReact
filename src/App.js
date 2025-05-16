2ec784a2c249b36bd76e97c2f6866675d2391ea3
brenobccc <brenobritobc@gmail.com>
2025-05-16 20:25:37 -0300

    Como requisitar em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
 
 function App() {
+  const [post, setPost] = useState([])
+  const baseURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(JSON.parse(JSON.stringify(response.data.results)))
+    });
+
+  }, []);
+
+  console.log(post)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        post.map((e) => (
+          <h1>
+            {e.name}
+          </h1>
+        ))
+      }
     </div>
   );
 }
