eaa156410fbbb54d541c4dfc43c84262d233ea70
brenobccc <brenobritobc@gmail.com>
2025-05-30 10:13:15 -0300

    alterando exemplo para pokemons de duas APIS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import axios from "axios";
 
 function App() {
   const [post, setPost] = useState([])
-  const baseURL = 'https://pokeapi.co/api/v2/pokemon'
+  const qtdPoke = 706
+  const baseURL = 'https://pokeapi.co/api/v2/pokemon?limit='+qtdPoke
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
@@ -20,11 +21,30 @@ function App() {
   return (
     <div className="App">
       {
-        post.map((e) => (
-          <h1>
-            {e.name}
-          </h1>
-        ))
+        post.map((e) => {
+
+          const pokeNumber = e.url.split('pokemon')[1].replaceAll('/','')
+          console.log(pokeNumber)
+          const urlFoto = 'https://raw.githubusercontent.com/wellrccity/pokedex-html-js/refs/heads/master/assets/img/pokemons/poke_'+pokeNumber+'.gif'
+          // const sitePoke = 'https://www.pokemon.com/br/pokedex/'+pokeNumber
+          return (
+            <div style={{
+              width: '300px',
+              height: '300px',
+              backgroundColor: 'orange',
+              margin: '5px'
+            }}>
+              <h4>
+                {pokeNumber}
+              </h4>
+              {/* <a href={sitePoke}>poke</a> */}
+              <img src={urlFoto} style={{
+                width: '40%',
+                position: 'cover'
+              }}></img>
+            </div>
+          )
+        })
       }
     </div>
   );
